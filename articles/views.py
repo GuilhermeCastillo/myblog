7eb6087450288a5191cd1309580243ba863bfffd
castilho <guilherme.castilhot2c18@gmail.com>
2024-02-09 22:29:48 -0300

    form get button search

--- articles/views.py
+++ articles/views.py
@@ -5,5 +5,9 @@ from articles.models import Article
 # Create your views here.
 def articles_view(request):
     articles = Article.objects.all()
-      
+    search = request.GET.get("search")
+
+    if search:
+        articles = Article.objects.filter(title__icontains=search)
+
     return render(request, "articles.html", {"articles": articles})
